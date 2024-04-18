e5723afe23dce2b9250e6368de3d3c409f08f189
wxy <1939311091@qq.com>
2024-04-18 11:28:16 +0800

    fix

--- generate_readme.py
+++ generate_readme.py
@@ -3,22 +3,24 @@ import os
 # GitHub 用户名和仓库名
 github_user = 'palp1tate'
 github_repo = 'AcWing'
-base_url = f"https://github.com/{github_user}/{github_repo}/blob/master/markdown/"
+answer_url = f"https://github.com/{github_user}/{github_repo}/blob/master/markdown/"
 
 # 获取markdown目录下的所有Markdown文件
 markdown_dir = 'markdown'
 files = [f for f in os.listdir(markdown_dir) if os.path.isfile(os.path.join(markdown_dir, f)) and f.endswith('.md')]
 
 # 生成Markdown表格
-table_rows = ["| 题目 | 链接 |"]
-table_rows.append("| --- | --- |")
+table_rows = ["| 原题 | 题解 |", "| --- | --- |"]
 for file in files:
-    file_path = os.path.join(base_url, file)
-    table_rows.append(f"| {file[:-3]} | [{file}]({file_path}) |")  # 假设文件名没有扩展名，如你的题解文件名为'001.md'
+    file_path = os.path.join(answer_url, file)
+    # 读取文件的第一行来获取题目链接
+    with open(os.path.join(markdown_dir, file), 'r', encoding='utf-8') as md_file:
+        first_line = md_file.readline().strip()
+    table_rows.append(f"| {first_line} | [{file}](<{file_path}>) |")
 
 # 组装最终的README内容
-readme_content = "# 题解目录\n\n" + "\n".join(table_rows) + "\n"
+readme_content = "# AcWing题解目录\n\n" + "\n".join(table_rows) + "\n"
 
 # 写入README.md到根目录
-with open('README.md', 'w') as f:
+with open('README.md', 'w', encoding='utf-8') as f:
     f.write(readme_content)
